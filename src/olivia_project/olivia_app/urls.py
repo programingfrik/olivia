f7057e6f44deae295e6143cd6fced9e1716fa195
Pablo Mercader Alcántara <programingfrik@gmail.com>
2026-02-17 17:25:21 -0400

    Ahora tenemos un indice que hace una lista con los contactos.

--- src/olivia_project/olivia_app/urls.py
+++ src/olivia_project/olivia_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name = "index"),
+    path("contacto/<int:contacto_id>/",
+         views.detalle_cont, name = "detalle_cont"),
     path("<int:usuario_id>/", views.index, name = "index"),
     path("<int:usuario_id>/contacto/<int:contacto_id>/",
          views.detalle_cont, name = "detalle_cont"),
--- src/olivia_project/olivia_app/views.py
+++ src/olivia_project/olivia_app/views.py
@@ -1,10 +1,18 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Persona
 
 # Create your views here.
 
 def index(request, usuario_id = None):
-    return HttpResponse("Este es el indice de contactos %s." % usuario_id)
+    contactos = Persona.objects.order_by("nacimiento")
+    #salida = ", ".join([p.nombres + " " + p.apellidos for p in contactos])
+    #return HttpResponse(salida)
+    template = loader.get_template("olivia_app/index.html")
+    context = {"contactos": contactos}
+    return HttpResponse(template.render(context, request))
 
 def detalle_cont(request, usuario_id = None, contacto_id = None):
     return HttpResponse("Este es el detalle de un contacto %s. Que es un contacto de %s." % (contacto_id, usuario_id))
